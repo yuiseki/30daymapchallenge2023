714009b928df8e13eecd54ab227b4689a337d43e
yuiseki <yuiseki@gmail.com>
2023-11-05 19:59:06 +0900

    try to fix Day 23

--- src/app/2023/11/23/page.tsx
+++ src/app/2023/11/23/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import "/public/cesium/Widgets/widgets.css"
 
 const MyCesiumComponent = dynamic(() => import('./MyCesiumComponent').then(module => module.MyCesiumComponent), {
     ssr: false,
